46677523547a20f06822021baa6544e9b07aa818
WillieWillie419 <nsikanokon13@gmail.com>
2023-12-22 00:28:32 +0100

    Third-push

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
-  return <>Hello React</>;
+  return <div className="hello-react">Hello React</div>;
 };
 
 export default App;
